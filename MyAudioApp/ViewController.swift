c6b059352091ec12f664092b8b21d4aa1225b8b8
panshito02 <58049357+panshito02@users.noreply.github.com>
2020-06-26 07:49:20 -0600

    Initial Commit

---
+++ MyAudioApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAudioApp
+//
+//  Created by Francisco Higuera Zamarripa on 26/06/20.
+//  Copyright © 2020 Francisco Higuera Zamarripa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
